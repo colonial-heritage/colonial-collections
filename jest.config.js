df0ba829935483c920c59f184f7a658b9f769e55
sdevalk <sjors@sjorsdevalk.nl>
2023-01-29 16:11:55 +0100

    style: fixes

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const nextJest = require('next/jest');
 
 const createJestConfig = nextJest();
 
+/** @type {import('jest').Config} */
 const customJestConfig = {
   testMatch: ['**/*.test.ts(x)?'],
   collectCoverage: true,
--- jest.setup.js
+++ jest.setup.js
@@ -2,5 +2,6 @@ const {loadEnvConfig} = require('@next/env');
 
 // Make sure the environment variables for testing are loaded in the same way Next handles these
 module.exports = async () => {
-  loadEnvConfig(process.cwd());
+  const appDir = process.cwd();
+  loadEnvConfig(appDir);
 };
