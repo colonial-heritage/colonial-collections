dfb7dcc5a97886754d3f88174842014b8f97c0e7
Barbara Honhoff <bhonhoff@gmail.com>
2023-02-14 09:25:12 +0100

    Rename .txs files without jsx syntax to .ts

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const withNextIntl = require('next-intl/plugin')(
   // This is the default, also the `src` folder is supported out of the box
-  './src/i18n.tsx'
+  './src/i18n.ts'
 );
 
 const nextConfig = {
