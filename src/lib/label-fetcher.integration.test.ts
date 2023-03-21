1bae030ede5d8251f473364014e5d36f3f35dd92
sdevalk <sjors@sjorsdevalk.nl>
2023-03-21 11:28:51 +0100

    feat: label fetcher

---
+++ src/lib/label-fetcher.integration.test.ts
@@ -0,0 +1,87 @@
+import {LabelFetcher} from './label-fetcher';
+import {afterEach, describe, expect, it, jest} from '@jest/globals';
+import {env} from 'node:process';
+import {SparqlEndpointFetcher} from 'fetch-sparql-endpoint';
+
+const labelFetcher = new LabelFetcher({
+  endpointUrl: env.SEARCH_PLATFORM_SPARQL_ENDPOINT_URL as string,
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+describe('getLabels', () => {
+  it('does not return labels if no IDs were provided', async () => {
+    const labels = await labelFetcher.getLabels({
+      ids: [],
+    });
+
+    expect(labels.size).toBe(0);
+  });
+
+  it('returns undefined if the label of the provided ID does not exist', async () => {
+    const labels = await labelFetcher.getLabels({
+      ids: ['https://example.org'],
+    });
+
+    expect(labels.size).toBe(1);
+    expect(labels.get('https://example.org')).toBeUndefined();
+  });
+
+  it('gets the labels of the provided IDs', async () => {
+    const labels = await labelFetcher.getLabels({
+      ids: [
+        'https://hdl.handle.net/20.500.11840/termmaster10063182',
+        'https://hdl.handle.net/20.500.11840/termmaster10063351',
+      ],
+    });
+
+    expect(labels.size).toBe(2);
+    expect(
+      labels.get('https://hdl.handle.net/20.500.11840/termmaster10063182')
+    ).toBe('Jakarta');
+    expect(
+      labels.get('https://hdl.handle.net/20.500.11840/termmaster10063351')
+    ).toBe('Bali');
+  });
+});
+
+describe('getLabels', () => {
+  let sparqlEndpointFetcherSpy: jest.SpiedFunction<
+    SparqlEndpointFetcher['fetchBindings']
+  >;
+
+  beforeEach(() => {
+    sparqlEndpointFetcherSpy = jest.spyOn(
+      // @ts-expect-error:TS2345
+      labelFetcher.fetcher,
+      'fetchBindings'
+    );
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('gets the labels of the provided IDs from the SPARQL endpoint', async () => {
+    await labelFetcher.getLabels({
+      ids: ['https://hdl.handle.net/20.500.11840/termmaster10058074'],
+    });
+
+    expect(sparqlEndpointFetcherSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('gets the labels of the provided IDs from the cache', async () => {
+    await labelFetcher.getLabels({
+      ids: ['https://hdl.handle.net/20.500.11840/termmaster10058074'],
+    });
+
+    // Request the labels again; should not trigger a call to the SPARQL endpoint
+    await labelFetcher.getLabels({
+      ids: ['https://hdl.handle.net/20.500.11840/termmaster10058074'],
+    });
+
+    expect(sparqlEndpointFetcherSpy).not.toHaveBeenCalled();
+  });
+});
---
+++ src/lib/label-fetcher.ts
@@ -0,0 +1,84 @@
+import arrayifyStream from 'arrayify-stream';
+import {SparqlEndpointFetcher} from 'fetch-sparql-endpoint';
+import LRUCache from 'lru-cache';
+import {z} from 'zod';
+
+const constructorOptionsSchema = z.object({
+  endpointUrl: z.string(),
+});
+
+export type ConstructorOptions = z.infer<typeof constructorOptionsSchema>;
+
+const searchOptionsSchema = z.object({
+  ids: z.array(z.string().url()),
+});
+
+export type SearchOptions = z.infer<typeof searchOptionsSchema>;
+
+export type Labels = Map<string, string | undefined>;
+
+// Fetches labels of IDs from a SPARQL endpoint, for display to users
+export class LabelFetcher {
+  private endpointUrl: string;
+  private fetcher = new SparqlEndpointFetcher();
+  private cache: LRUCache<string, string> = new LRUCache({max: 10000});
+
+  constructor(options: ConstructorOptions) {
+    const opts = constructorOptionsSchema.parse(options);
+    this.endpointUrl = opts.endpointUrl;
+  }
+
+  private getIdsToQuery(ids: string[]) {
+    // Remove duplicate IDs
+    const uniqueIds = [...new Set(ids)];
+
+    // Remove IDs already cached
+    const uniqueAndUncachedIds = uniqueIds.filter(
+      (id: string) => !this.cache.has(id)
+    );
+
+    return uniqueAndUncachedIds;
+  }
+
+  // TBD: call this repeatedly if the endpoint limits its results?
+  private async queryAndCacheLabels(ids: string[]) {
+    if (ids.length === 0) {
+      return; // No IDs to query
+    }
+
+    const queryConditions = ids.map((id: string) => {
+      return `{
+        BIND(<${id}> AS ?id)
+        ?id cc:name ?label
+      }`;
+    });
+
+    const query = `PREFIX cc: <https://colonialcollections.nl/search#>
+      SELECT ?id ?label WHERE {
+        ${queryConditions.join(' UNION ')}
+      }`;
+
+    const bindingsStream = await this.fetcher.fetchBindings(
+      this.endpointUrl,
+      query
+    );
+
+    const bindings = await arrayifyStream(bindingsStream);
+    for (const binding of bindings) {
+      this.cache.set(binding.id.value, binding.label.value);
+    }
+  }
+
+  async getLabels(options: SearchOptions): Promise<Labels> {
+    const opts = searchOptionsSchema.parse(options);
+
+    const ids = this.getIdsToQuery(opts.ids);
+    await this.queryAndCacheLabels(ids);
+
+    // Return only the labels of the requested IDs
+    const labels: Labels = new Map();
+    opts.ids.forEach((id: string) => labels.set(id, this.cache.get(id)));
+
+    return labels;
+  }
+}
