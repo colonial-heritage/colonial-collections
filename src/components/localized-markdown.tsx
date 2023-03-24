06661825de92a6852d3db4c82a9c9367f8311e48
Barbara Honhoff <bhonhoff@gmail.com>
2023-03-24 10:26:19 +0100

    Workaround async components with typescript

--- src/components/localized-markdown.tsx
+++ src/components/localized-markdown.tsx
@@ -1,7 +1,11 @@
 import {useLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 
-export default async function LocalizedMarkdown({name}: {name: string}) {
+interface Props {
+  name: string;
+}
+
+async function LocalizedMarkdown({name}: Props) {
   const locale = useLocale();
   try {
     const Markdown = (await import(`@/messages/${locale}/${name}.mdx`)).default;
@@ -10,3 +14,10 @@ export default async function LocalizedMarkdown({name}: {name: string}) {
     notFound();
   }
 }
+
+// TypeScript doesn't understand async components yet.
+// So this is a temporary workaround.
+// More info:
+//  - Next.js issue: https://github.com/vercel/next.js/issues/42292
+//  - Typescript pull request: https://github.com/microsoft/TypeScript/pull/51328
+export default LocalizedMarkdown as unknown as ({name}: Props) => JSX.Element;
