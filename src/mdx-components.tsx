a007b21b24ae8b1f1b24a20274582d3a07242915
BasD <basdoppen@gmail.com>
2023-03-24 16:41:01 +0100

    md markup

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -3,20 +3,21 @@ import type {MDXComponents} from 'mdx/types';
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({children}) => (
-      <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+      <h1 className="font-bold leading-tight tracking-tight text-gray-900">
         {children}
       </h1>
     ),
     h2: ({children}) => (
-      <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900">
+      <h1 className="font-bold leading-tight tracking-tight text-gray-900">
         {children}
       </h1>
     ),
     h3: ({children}) => (
-      <h1 className="text-1xl font-bold leading-tight tracking-tight text-gray-900">
+      <h1 className="font-bold leading-tight tracking-tight text-gray-900">
         {children}
       </h1>
     ),
+    p: ({children}) => <p className="text-gray-900 w-96">{children}</p>,
     ...components,
   };
 }
