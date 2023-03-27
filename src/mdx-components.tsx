0fc20b06fcadabe780a73d83c100bb83af69f7d6
BasD <basdoppen@gmail.com>
2023-03-27 21:15:39 +0200

    link + headings

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -8,14 +8,14 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       </h1>
     ),
     h2: ({children}) => (
-      <h1 className="font-bold leading-tight tracking-tight text-gray-900">
+      <h2 className="font-bold leading-tight tracking-tight text-gray-900">
         {children}
-      </h1>
+      </h2>
     ),
     h3: ({children}) => (
-      <h1 className="font-bold leading-tight tracking-tight text-gray-900">
+      <h3 className="font-bold leading-tight tracking-tight text-gray-900">
         {children}
-      </h1>
+      </h3>
     ),
     p: ({children}) => <p className="text-gray-900 w-96">{children}</p>,
     ...components,
