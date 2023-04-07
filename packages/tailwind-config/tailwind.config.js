a1748e604fdd7a1d50c7581d5715087c7825b927
Barbara Honhoff <bhonhoff@gmail.com>
2023-04-07 10:51:31 +0200

    Re-add unintentional removed tailwind config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['src/**/*.{js,ts,jsx,tsx}', '../../packages/ui/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.900'),
+          },
+        },
+      }),
       colors: {
         gray: {
           light: '#F7F5F2',
