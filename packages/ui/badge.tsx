708a15a46e093c98eeea9271dc81f1b298a07258
Barbara Honhoff <bhonhoff@gmail.com>
2023-04-13 08:46:01 +0200

    Add icons to dataset card

--- packages/ui/badge.tsx
+++ packages/ui/badge.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export function Badge({children, variant = 'transparent', testId}: Props) {
   const variantClassName = classNames(
-    'inline-flex items-center rounded-md mr-4 px-2 py-1',
+    'inline-flex items-center rounded-md mr-3 mb-1 px-2 py-1',
     {
       'bg-gray-medium text-xs text-gray-900': variant === 'transparent',
       'text-sm bg-stone-100': variant === 'gray',
@@ -21,6 +21,19 @@ export function Badge({children, variant = 'transparent', testId}: Props) {
       {children}
     </span>
   );
+}
+
+interface BadgeIconProps {
+  Icon?: React.ElementType;
+  variant?: 'outline' | 'solid';
+}
+
+function BadgeIcon({Icon = XMarkIcon, variant = 'outline'}: BadgeIconProps) {
+  const variantClassName = classNames('h-5 w-5 -ml-1 mr-1', {
+    'stroke-stone-400': variant === 'outline',
+    'fill-stone-400': variant === 'solid',
+  });
+  return <Icon className={variantClassName} />;
 }
 
 interface BadgeActionProps {
@@ -41,3 +54,4 @@ function BadgeAction({Icon = XMarkIcon, onClick}: BadgeActionProps) {
 }
 
 Badge.Action = BadgeAction;
+Badge.Icon = BadgeIcon;
