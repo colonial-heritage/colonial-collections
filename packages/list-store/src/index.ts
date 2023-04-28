b5e84c9258236fe1eb3ca4c27b70697e9ef9249e
Barbara Honhoff <bhonhoff@gmail.com>
2023-04-28 12:22:07 +0200

    Clarify some comments

--- packages/list-store/src/index.ts
+++ packages/list-store/src/index.ts
@@ -1,6 +1,6 @@
 export * from './useListStore';
 export * from './sort';
-// Explicitly reexported client components.
-// https://github.com/vercel/next.js/issues/41940#issuecomment-1480885131
+// Explicitly reexported client components to prevent error "Error: Unsupported Server Component type: undefined".
+// See: https://github.com/vercel/next.js/issues/41940#issuecomment-1480885131
 export {ClientListStore} from './client-list-store';
 export * from './useSearchParamsUpdate';
