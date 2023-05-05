f7d972e097c14dfa858b18503c1701e407e67fc3
Barbara Honhoff <bhonhoff@gmail.com>
2023-05-05 10:16:04 +0200

    Move measurement svg to separate component

--- apps/dataset-browser/src/app/[locale]/dataset-card.tsx
+++ apps/dataset-browser/src/app/[locale]/dataset-card.tsx
@@ -7,6 +7,7 @@ import {
   BuildingLibraryIcon,
 } from '@heroicons/react/24/solid';
 import {DocumentCheckIcon, TagIcon} from '@heroicons/react/24/outline';
+import BooleanMeasurement from '@/components/boolean-measurement';
 
 export default function DatasetCard({dataset}: {dataset: Dataset}) {
   const t = useTranslations('DatasetCard');
@@ -39,37 +40,7 @@ export default function DatasetCard({dataset}: {dataset: Dataset}) {
                 {measurement.metric.name}
               </div>
               <div className="flex flex-col items-center justify-end h-full w-full shrink">
-                {measurement.value ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="stroke-sky-500 w-5 h-5 stroke-[5px]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4.5 12.75l6 6 9-13.5"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="stroke-gray-300 w-5 h-5 stroke-[5px]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
+                <BooleanMeasurement value={measurement.value} />
               </div>
             </div>
           ))}
