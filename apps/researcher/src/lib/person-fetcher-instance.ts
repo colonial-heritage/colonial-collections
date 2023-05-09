08e545dd63ec9e8591c369296850b00bfbdd9ead
sdevalk <sjors@sjorsdevalk.nl>
2023-05-09 22:28:04 +0200

    person fetcher instance

---
+++ apps/researcher/src/lib/person-fetcher-instance.ts
@@ -0,0 +1,14 @@
+import {LabelFetcher} from '@colonial-collections/label-fetcher';
+import {PersonFetcher} from '@/lib/persons';
+import {env} from 'node:process';
+
+const labelFetcher = new LabelFetcher({
+  endpointUrl: env.SEARCH_PLATFORM_SPARQL_ENDPOINT_URL as string,
+});
+
+const personFetcher = new PersonFetcher({
+  endpointUrl: env.SEARCH_PLATFORM_ELASTIC_ENDPOINT_URL as string,
+  labelFetcher,
+});
+
+export default personFetcher;
