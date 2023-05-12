0202cb212752644a24496be9370d87e41577ad86
sdevalk <sjors@sjorsdevalk.nl>
2023-05-12 07:46:20 +0200

    typo

--- apps/researcher/src/lib/objects/fetcher-request.ts
+++ apps/researcher/src/lib/objects/fetcher-request.ts
@@ -1,7 +1,7 @@
 export function buildAggregation(id: string) {
   const aggregation = {
     terms: {
-      size: 100, // TBD: what's a good size?,
+      size: 1000, // TBD: what's a good size?
       field: `${id}.keyword`,
     },
   };
