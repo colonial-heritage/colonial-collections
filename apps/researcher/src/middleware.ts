54941b33a928bdabd5f91a6b004b3774f35db026
Barbara Honhoff <bhonhoff@gmail.com>
2023-07-03 13:39:22 +0200

    Fix the middleware, set the correct request headers

--- apps/researcher/src/middleware.ts
+++ apps/researcher/src/middleware.ts
@@ -1,5 +1,5 @@
 import createIntlMiddleware from 'next-intl/middleware';
-import type {NextRequest} from 'next/server';
+import {NextRequest} from 'next/server';
 import {authMiddleware} from '@clerk/nextjs';
 
 // Set the available locales here. These values should match a .json file in /messages.
@@ -8,20 +8,34 @@ import {authMiddleware} from '@clerk/nextjs';
 // So you can't read the filesystem.
 export const locales = ['en', 'nl'];
 
+const publicRoutes = ['/', '/:locale', '/:locale/sign-in', '/:locale/sign-up'];
+
 export const config = {
   // Skip all internal paths
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
 
-const intlMiddleware = createIntlMiddleware({
-  locales,
-  defaultLocale: 'en',
-});
-
 export default authMiddleware({
-  beforeAuth: req => {
-    return intlMiddleware(req);
-  },
+  beforeAuth(request: NextRequest) {
+    // This middleware intercepts requests to `/` and will redirect
+    // to one of the configured locales instead (e.g. `/en`).
+    // In the background a cookie is set that will remember the
+    // locale of the last page that the user has visited.
+    // The middleware furthermore passes the resolved locale
+    // to components in your app.
+    const handleI18nRouting = createIntlMiddleware({
+      locales,
+      defaultLocale: 'en',
+    });
 
-  publicRoutes: ['/', '/:locale', '/:locale/sign-in', '/:locale/sign-up'],
+    // Store current request pathname in the request header,
+    // this can be used to set the active menu/tab item.
+    // See issue: https://github.com/vercel/next.js/issues/43704
+    request.headers.set('x-pathname', request.nextUrl.pathname);
+
+    const response = handleI18nRouting(request);
+
+    return response;
+  },
+  publicRoutes,
 });
