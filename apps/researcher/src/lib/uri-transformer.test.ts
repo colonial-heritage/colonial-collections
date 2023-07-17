d63cfea80f103799b0947b8bb36fb54bd75d166e
Barbara Honhoff <bhonhoff@gmail.com>
2023-07-17 21:21:05 +0200

    Processed minor code review comments

--- apps/researcher/src/lib/uri-transformer.test.ts
+++ apps/researcher/src/lib/uri-transformer.test.ts
@@ -2,25 +2,15 @@ import {describe, expect, it} from '@jest/globals';
 import {encodeUri, decodeUri} from './uri-transformer';
 
 describe('encodeUri', () => {
-  it('encodes an url', () => {
+  it('encodes a URI', () => {
     const encodedUri = encodeUri('https://museum.example.org/');
     expect(encodedUri).toBe('https%3A%2F%2Fmuseum%252Eexample%252Eorg%2F');
-  });
-
-  it('encodes a string with spaces', () => {
-    const encodedUri = encodeUri('A string with spaces');
-    expect(encodedUri).toBe('A%20string%20with%20spaces');
   });
 });
 
 describe('decodeUri', () => {
-  it('decodes an url', () => {
+  it('decodes a URI', () => {
     const decodedUri = decodeUri('https%3A%2F%2Fmuseum%252Eexample%252Eorg%2F');
     expect(decodedUri).toBe('https://museum.example.org/');
-  });
-
-  it('decodes a string with spaces', () => {
-    const decodedUri = decodeUri('A%20string%20with%20spaces');
-    expect(decodedUri).toBe('A string with spaces');
   });
 });
--- apps/researcher/src/lib/uri-transformer.ts
+++ apps/researcher/src/lib/uri-transformer.ts
@@ -1,5 +1,6 @@
 // To use the URIs in dynamic routes, we need to encode them first.
 export function encodeUri(uri: string) {
+  // If the dynamic routes segments contain dots, the Clerk middleware will throw an error, so encode the dots.
   return encodeURIComponent(uri.replaceAll('.', '%2E'));
 }
 
