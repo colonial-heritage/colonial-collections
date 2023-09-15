10672fd5208f8113edfc3caeef1b2fbe6762261a
Barbara Honhoff <bhonhoff@gmail.com>
2023-09-15 16:51:16 +0200

    Create separate database migrations for the separate tables

---
+++ packages/datahub-data/migrations/0000_create object_list.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `object_list` (
+  `id` serial AUTO_INCREMENT NOT NULL,
+  `name` varchar(256),
+  `description` text,
+  `community_id` varchar(256),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(256) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT `object_list_id` PRIMARY KEY(`id`)
+);
+--> statement-breakpoint
+CREATE INDEX `community_id` ON `object_list` (`community_id`);
