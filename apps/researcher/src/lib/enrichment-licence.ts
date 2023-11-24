a3ae9c825d93c5ae432a114e535560669c30d804
Barbara Honhoff <bhonhoff@gmail.com>
2023-11-24 14:03:30 +0100

    Rename environment variable to a public name

--- apps/researcher/src/lib/enrichment-licence.ts
+++ apps/researcher/src/lib/enrichment-licence.ts
@@ -1,7 +1,8 @@
-if (!process.env['COMMUNITY_ENRICHMENT_LICENSE']) {
+if (!process.env['NEXT_PUBLIC_COMMUNITY_ENRICHMENT_LICENSE']) {
   throw new Error(
-    'COMMUNITY_ENRICHMENT_LICENSE is not defined or invalid in the environment'
+    'NEXT_PUBLIC_COMMUNITY_ENRICHMENT_LICENSE is not defined or invalid in the environment'
   );
 }
 
-export const enrichmentLicence = process.env['COMMUNITY_ENRICHMENT_LICENSE'];
+export const enrichmentLicence =
+  process.env['NEXT_PUBLIC_COMMUNITY_ENRICHMENT_LICENSE'];
