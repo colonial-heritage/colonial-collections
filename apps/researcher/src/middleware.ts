37cdea933780afefb8d8d39b2b366d8f550e936e
Barbara Honhoff <bhonhoff@gmail.com>
2023-11-29 16:21:38 +0100

    Simplefy the middleware

--- apps/researcher/src/middleware.ts
+++ apps/researcher/src/middleware.ts
@@ -9,30 +9,28 @@ export const locales = ['en', 'nl'];
 
 export const config = {
   // Skip all internal paths
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// This middleware intercepts requests to `/` and will redirect
+// to one of the configured locales instead (e.g. `/en`).
+// In the background a cookie is set that will remember the
+// locale of the last page that the user has visited.
+// The middleware furthermore passes the resolved locale
+// to components.
+const intlMiddleware = createIntlMiddleware({
+  locales,
+  defaultLocale: 'en',
+});
 
 export default authMiddleware({
   beforeAuth(request) {
-    // This middleware intercepts requests to `/` and will redirect
-    // to one of the configured locales instead (e.g. `/en`).
-    // In the background a cookie is set that will remember the
-    // locale of the last page that the user has visited.
-    // The middleware furthermore passes the resolved locale
-    // to components.
-    const handleI18nRouting = createIntlMiddleware({
-      locales,
-      defaultLocale: 'en',
-    });
-
     // Store current request pathname in the request header,
     // this can be used to set the active menu/tab item.
     // See issue: https://github.com/vercel/next.js/issues/43704
-    request.headers.set('x-pathname', request.nextUrl.pathname);
+    // request.headers.set('x-pathname', request.nextUrl.pathname);
 
-    const response = handleI18nRouting(request);
-
-    return response;
+    return intlMiddleware(request);
   },
   publicRoutes: ['/', '/(.*)'],
   debug: true,
