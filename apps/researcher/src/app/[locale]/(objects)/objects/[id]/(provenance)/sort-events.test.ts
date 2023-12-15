53a3901b22ae8c71dc86c29ad0b36a3ac0f17b07
Barbara Honhoff <bhonhoff@gmail.com>
2023-12-15 10:41:37 +0100

    Refactored sort-events.ts

--- apps/researcher/src/app/[locale]/(objects)/objects/[id]/(provenance)/sort-events.test.ts
+++ apps/researcher/src/app/[locale]/(objects)/objects/[id]/(provenance)/sort-events.test.ts
@@ -4,20 +4,42 @@ import {describe, expect} from '@jest/globals';
 describe('sortEvents', () => {
   it('sorts events based on `startsAfter` and `endsBefore`', () => {
     const events = [
-      {id: '5', startsAfter: '3', endsBefore: null},
-      {id: '3', startsAfter: '1', endsBefore: '4'},
+      {id: '5', startsAfter: '4', endsBefore: null},
+      {id: '3', startsAfter: '2', endsBefore: '4'},
       {id: '4', startsAfter: '3', endsBefore: '5'},
-      {id: '1', startsAfter: null, endsBefore: null},
+      {id: '1', startsAfter: null, endsBefore: '2'},
       {id: '2', startsAfter: '1', endsBefore: '3'},
     ];
     // @ts-expect-error:TS2322
     const sorted = sortEvents(events);
     expect(sorted).toEqual([
-      {id: '1', startsAfter: null, endsBefore: null},
+      {id: '1', startsAfter: null, endsBefore: '2'},
       {id: '2', startsAfter: '1', endsBefore: '3'},
-      {id: '3', startsAfter: '1', endsBefore: '4'},
+      {id: '3', startsAfter: '2', endsBefore: '4'},
       {id: '4', startsAfter: '3', endsBefore: '5'},
-      {id: '5', startsAfter: '3', endsBefore: null},
+      {id: '5', startsAfter: '4', endsBefore: null},
+    ]);
+  });
+
+  it('sorts an event that is placed in between two existing events`', () => {
+    const events = [
+      {id: '1', startsAfter: null, endsBefore: '2'},
+      {id: '2', startsAfter: '1', endsBefore: '3'},
+      {id: '3', startsAfter: '2', endsBefore: '4'},
+      {id: '4', startsAfter: '3', endsBefore: '5'},
+      {id: '5', startsAfter: '4', endsBefore: null},
+      // This event is placed in between two existing events
+      {id: '6', startsAfter: '2', endsBefore: '3'},
+    ];
+    // @ts-expect-error:TS2322
+    const sorted = sortEvents(events);
+    expect(sorted).toEqual([
+      {id: '1', startsAfter: null, endsBefore: '2'},
+      {id: '2', startsAfter: '1', endsBefore: '3'},
+      {id: '6', startsAfter: '2', endsBefore: '3'},
+      {id: '3', startsAfter: '2', endsBefore: '4'},
+      {id: '4', startsAfter: '3', endsBefore: '5'},
+      {id: '5', startsAfter: '4', endsBefore: null},
     ]);
   });
 });
--- apps/researcher/src/app/[locale]/(objects)/objects/[id]/(provenance)/sort-events.ts
+++ apps/researcher/src/app/[locale]/(objects)/objects/[id]/(provenance)/sort-events.ts
@@ -1,29 +1,35 @@
 import {ProvenanceEvent} from '@/lib/api/definitions';
 
-// This function sorts an array of events based on their `startsAfter` and `endsBefore` properties.
 export function sortEvents(events: ProvenanceEvent[]): ProvenanceEvent[] {
-  // `graph` is a Map where each key is an event id, and each value is an array of ids of events that start after the key event.
   const graph = new Map();
-  // `incomingEdgeCount` is a Map where each key is an event id, and each value is the number of events that start after the key event.
   const incomingEdgeCount = new Map();
 
-  // Initialize the graph and incomingEdgeCount map
+  // Step 1: Initialize the `graph` and `incomingEdgeCount` map
+  // Input: events array.
+  // Operation: For each event, create an empty array in the graph,
+  // and set the incoming edge count to 0.
+  // Output: `graph` and `incomingEdgeCount` maps initialized with each event id.
   events.forEach(event => {
-    // For each event, create an empty array in the graph, and set the incoming edge count to 0.
     graph.set(event.id, []);
     incomingEdgeCount.set(event.id, 0);
   });
 
-  // Build the graph and update incoming edge counts
+  // Step 2: Build the graph and update incoming edge counts
+  // Input: events array, graph and incomingEdgeCount maps.
+  // Operation: For each event:
+  // if it starts after another event,
+  // add an edge from the other event to this event in the graph,
+  // and increase the incoming edge count for this event.
+  // If it ends before another event,
+  // add an edge from this event to the other event in the graph,
+  // and increase the incoming edge count for the other event.
+  // Output: graph map with edges between events, incomingEdgeCount map with counts
+  // of incoming edges for each event.
   events.forEach(event => {
-    // If an event starts after another event, add an edge from the other event to this event in the graph,
-    // and increase the incoming edge count for this event.
     if (event.startsAfter) {
       graph.get(event.startsAfter).push(event.id);
       incomingEdgeCount.set(event.id, incomingEdgeCount.get(event.id) + 1);
     }
-    // If an event ends before another event, add an edge from this event to the other event in the graph,
-    // and increase the incoming edge count for the other event.
     if (event.endsBefore) {
       graph.get(event.id).push(event.endsBefore);
       incomingEdgeCount.set(
@@ -33,28 +39,35 @@ export function sortEvents(events: ProvenanceEvent[]): ProvenanceEvent[] {
     }
   });
 
-  // Perform a topological sort
-  // Start with a queue of all events that have no incoming edges (i.e., their incoming edge count is 0).
-  const queue = Array.from(incomingEdgeCount.entries())
+  // Step 3: Initialize the queue of events
+  // Input: `incomingEdgeCount` map.
+  // Operation: Create a queue of all events that have no incoming edges
+  // (i.e., their incoming edge count is 0).
+  // Output: `queueOfEvents` array with ids of events that have no incoming edges.
+  const queueOfEvents = Array.from(incomingEdgeCount.entries())
     .filter(([, count]) => count === 0)
     .map(([id]) => id);
 
-  const sorted = [];
-  while (queue.length > 0) {
-    // Remove an event from the queue, add it to the sorted array,
-    // and decrease the incoming edge count for all events that start after it.
-    const id = queue.shift();
-    sorted.push(events.find(event => event.id === id)!);
+  // Step 4: Perform a topological sort
+  // Input: `queueOfEvents` array, `graph` and `incomingEdgeCount` maps.
+  // Operation: While the queue is not empty, remove an event from the `queue`,
+  // add it to the sorted array,
+  // and decrease the incoming edge count for all events that start after it.
+  // If an event now has no incoming edges, add it to the `queue`.
+  // Output: `sortedEvents` array with events sorted based on their
+  // `startsAfter` and `endsBefore` properties.
+  const sortedEvents = [];
+  while (queueOfEvents.length > 0) {
+    const id = queueOfEvents.shift();
+    sortedEvents.push(events.find(event => event.id === id)!);
 
     graph.get(id).forEach((neighbor: string) => {
       incomingEdgeCount.set(neighbor, incomingEdgeCount.get(neighbor) - 1);
-      // If an event now has no incoming edges, add it to the queue.
       if (incomingEdgeCount.get(neighbor) === 0) {
-        queue.push(neighbor);
+        queueOfEvents.push(neighbor);
       }
     });
   }
 
-  // Return the sorted array of events
-  return sorted;
+  return sortedEvents;
 }
