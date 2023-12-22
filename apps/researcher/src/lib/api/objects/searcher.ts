9fea64516545073f31ab9490a4f30e2f7c69b273
sdevalk <sjors@sjorsdevalk.nl>
2023-12-22 06:47:02 +0100

    use SPARQL for fetching the search results

--- apps/researcher/src/lib/api/objects/searcher.ts
+++ apps/researcher/src/lib/api/objects/searcher.ts
@@ -1,22 +1,17 @@
 import {
-  Dataset,
-  HeritageObject,
-  Image,
-  Person,
   SearchResultFilter,
   SortBy,
   SortByEnum,
   SortOrder,
   SortOrderEnum,
-  Term,
 } from '../definitions';
 import {SearchResult} from './definitions';
-import {removeNullish} from '../rdf-helpers';
-import {reach} from '@hapi/hoek';
+import {HeritageObjectFetcher} from './fetcher';
 import {z} from 'zod';
 
 const constructorOptionsSchema = z.object({
   endpointUrl: z.string(),
+  heritageObjectFetcher: z.instanceof(HeritageObjectFetcher),
 });
 
 export type ConstructorOptions = z.infer<typeof constructorOptionsSchema>;
@@ -25,20 +20,15 @@ enum RawKeys {
   Id = '@id',
   Type = 'http://www w3 org/1999/02/22-rdf-syntax-ns#type',
   AdditionalType = 'https://colonialcollections nl/schema#additionalType',
-  Identifier = 'https://colonialcollections nl/schema#identifier',
   Name = 'https://colonialcollections nl/schema#name',
-  Description = 'https://colonialcollections nl/schema#description',
-  Inscription = 'https://colonialcollections nl/schema#inscription',
   About = 'https://colonialcollections nl/schema#about',
   Creator = 'https://colonialcollections nl/schema#creator',
   Material = 'https://colonialcollections nl/schema#material',
   Technique = 'https://colonialcollections nl/schema#technique',
-  Image = 'https://colonialcollections nl/schema#image',
   Publisher = 'https://colonialcollections nl/schema#publisher',
   YearCreatedStart = 'https://colonialcollections nl/schema#yearCreatedStart',
   YearCreatedEnd = 'https://colonialcollections nl/schema#yearCreatedEnd',
   CountryCreated = 'https://colonialcollections nl/schema#countryCreated',
-  IsPartOf = 'https://colonialcollections nl/schema#isPartOf',
 }
 
 const sortByToRawKeys = new Map<string, string>([
@@ -69,23 +59,7 @@ const searchOptionsSchema = z.object({
 
 export type SearchOptions = z.input<typeof searchOptionsSchema>;
 
-const rawHeritageObjectSchema = z
-  .object({})
-  .setKey(RawKeys.Id, z.string())
-  .setKey(RawKeys.AdditionalType, z.array(z.string()).optional())
-  .setKey(RawKeys.Identifier, z.array(z.string()).optional())
-  .setKey(RawKeys.Name, z.array(z.string()).optional())
-  .setKey(RawKeys.Description, z.array(z.string()).optional())
-  .setKey(RawKeys.Inscription, z.array(z.string()).optional())
-  .setKey(RawKeys.About, z.array(z.string()).optional())
-  .setKey(RawKeys.Creator, z.array(z.string()).optional())
-  .setKey(RawKeys.Material, z.array(z.string()).optional())
-  .setKey(RawKeys.Technique, z.array(z.string()).optional())
-  .setKey(RawKeys.Image, z.array(z.string()).optional())
-  .setKey(RawKeys.Publisher, z.array(z.string()).optional())
-  .setKey(RawKeys.IsPartOf, z.array(z.string()).min(1));
-
-type RawHeritageObject = z.infer<typeof rawHeritageObjectSchema>;
+const rawHeritageObjectSchema = z.object({}).setKey(RawKeys.Id, z.string());
 
 const rawBucketSchema = z.object({
   key: z.string().or(z.number()),
@@ -132,11 +106,13 @@ type RawSearchResponseWithAggregations = z.infer<
 
 export class HeritageObjectSearcher {
   private endpointUrl: string;
+  private heritageObjectFetcher: HeritageObjectFetcher;
 
   constructor(options: ConstructorOptions) {
     const opts = constructorOptionsSchema.parse(options);
 
     this.endpointUrl = opts.endpointUrl;
+    this.heritageObjectFetcher = opts.heritageObjectFetcher;
   }
 
   async makeRequest<T>(searchRequest: Record<string, unknown>): Promise<T> {
@@ -157,85 +133,6 @@ export class HeritageObjectSearcher {
     const responseData: T = await response.json();
 
     return responseData;
-  }
-
-  // Map the response to our internal model
-  private fromRawHeritageObjectToHeritageObject(
-    rawHeritageObject: RawHeritageObject
-  ) {
-    const name = reach(rawHeritageObject, `${RawKeys.Name}.0`);
-    const identifier = reach(rawHeritageObject, `${RawKeys.Identifier}.0`);
-    const description = reach(rawHeritageObject, `${RawKeys.Description}.0`);
-    const inscriptions = reach(rawHeritageObject, `${RawKeys.Inscription}`);
-    const publisherName = reach(rawHeritageObject, `${RawKeys.Publisher}.0`);
-    const datasetName = reach(rawHeritageObject, `${RawKeys.IsPartOf}.0`);
-    const dataset: Dataset = {
-      id: datasetName,
-      name: datasetName,
-      publisher: {
-        type: 'Organization', // TODO: determine dynamically - could also be a 'Person'
-        id: publisherName,
-        name: publisherName,
-      },
-    };
-
-    // Replace 'names' with IRIs as soon as we have IRIs
-    const toThings = <T>(rawKey: string) => {
-      const names: string[] | undefined = reach(rawHeritageObject, `${rawKey}`);
-      if (names === undefined) {
-        return undefined;
-      }
-
-      const things = names.map(name => {
-        return {
-          id: name,
-          name,
-        };
-      });
-
-      return things as T[];
-    };
-
-    const types = toThings<Term>(RawKeys.AdditionalType);
-    const subjects = toThings<Term>(RawKeys.About);
-    const materials = toThings<Term>(RawKeys.Material);
-    const techniques = toThings<Term>(RawKeys.Technique);
-    const creators = toThings<Person>(RawKeys.Creator); // TODO: set type (Person or Organization)
-
-    let images: Image[] | undefined;
-    const contentUrls: string[] | undefined = reach(
-      rawHeritageObject,
-      `${RawKeys.Image}`
-    );
-    if (contentUrls !== undefined) {
-      images = contentUrls.map(contentUrl => {
-        return {
-          id: contentUrl, // At this moment the same
-          contentUrl,
-        };
-      });
-    }
-
-    const heritageObjectWithUndefinedValues: HeritageObject = {
-      id: rawHeritageObject[RawKeys.Id],
-      name,
-      identifier,
-      description,
-      inscriptions,
-      types,
-      subjects,
-      materials,
-      techniques,
-      creators,
-      images,
-      isPartOf: dataset,
-    };
-
-    const heritageObject = removeNullish<HeritageObject>(
-      heritageObjectWithUndefinedValues
-    );
-
-    return heritageObject;
   }
 
   private buildAggregation(id: string) {
@@ -286,7 +183,7 @@ export class HeritageObjectSearcher {
             {
               // Only return documents of a specific type
               terms: {
-                [`${RawKeys.Type}.keyword`]: [
+                [`${RawKeys.Type}.keyword` as string]: [
                   'https://colonialcollections.nl/schema#HeritageObject',
                 ],
               },
@@ -369,10 +266,11 @@ export class HeritageObjectSearcher {
   ) {
     const {hits, aggregations} = rawSearchResponse;
 
-    const heritageObjects: HeritageObject[] = hits.hits.map(hit => {
-      const rawHeritageObject = hit._source;
-      return this.fromRawHeritageObjectToHeritageObject(rawHeritageObject);
-    });
+    const rawHeritageObjects = hits.hits.map(hit => hit._source);
+    const ids = rawHeritageObjects.map(
+      rawHeritageObject => rawHeritageObject['@id']
+    );
+    const heritageObjects = await this.heritageObjectFetcher.getByIds(ids);
 
     const typeFilters = this.buildFilters(aggregations.types.buckets);
     const subjectFilters = this.buildFilters(aggregations.subjects.buckets);
