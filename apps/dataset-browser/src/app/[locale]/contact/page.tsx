60313d06b78f66263fd36d3a123f0d2a1d86f063
Barbara Honhoff <barbara@honhoff.dev>
2024-01-17 12:10:14 +0100

    Add contact form to dataset-browser

--- apps/dataset-browser/src/app/[locale]/contact/page.tsx
+++ apps/dataset-browser/src/app/[locale]/contact/page.tsx
@@ -1,10 +1,16 @@
-import {LocalizedMarkdown} from '@colonial-collections/ui';
+import {LocalizedMarkdown, Notifications} from '@colonial-collections/ui';
+import {ContactForm} from '@colonial-collections/email-sender';
 
 export default function Contact() {
   return (
-    <LocalizedMarkdown
-      name="contact"
-      contentPath="@colonial-collections/content"
-    />
+    <>
+      <LocalizedMarkdown
+        name="contact"
+        contentPath="@colonial-collections/content"
+      />
+
+      <Notifications />
+      <ContactForm />
+    </>
   );
 }
