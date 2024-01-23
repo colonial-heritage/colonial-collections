87733805eca60473621f343bf23ea502f417287c
Barbara Honhoff <barbara@honhoff.dev>
2024-01-23 13:46:17 +0100

    Show placeholder if there is no object title

--- apps/researcher/src/app/[locale]/objects/heritage-object-card.tsx
+++ apps/researcher/src/app/[locale]/objects/heritage-object-card.tsx
@@ -35,7 +35,9 @@ export default function HeritageObjectCard({heritageObject}: Props) {
           className="font-semibold mt-4 text-consortiumGreen-300"
           data-testid="object-card-name"
         >
-          {heritageObject.name}
+          {heritageObject.name || (
+            <span className="italic font-normal">{t('noName')}</span>
+          )}
         </div>
         <div className="text-sm opacity-70">
           {heritageObject.isPartOf?.publisher?.name}
