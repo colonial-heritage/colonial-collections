e78db951ab06cb23417fdc260508974b6cd91db4
Barbara Honhoff <barbara@honhoff.dev>
2024-01-24 10:17:26 +0100

    Changed comment about SignedIn component

--- apps/researcher/src/lib/community/signed-in.tsx
+++ apps/researcher/src/lib/community/signed-in.tsx
@@ -5,9 +5,9 @@ import {ReactNode} from 'react';
 
 /*
 Use this component to wrap content that should only be visible to signed in users.
-Do not use the <SingIn /> component from @clerk/nextjs directly,
+Do not use the <SignedIn /> component from @clerk/nextjs directly,
 as it will sometimes not work in server components.
-By wrapping it in this client component, we can ensure that it will always work.
+By wrapping it in this client component it always works.
 */
 export default function SignedIn({children}: {children: ReactNode}) {
   return <ClerkSignedIn>{children}</ClerkSignedIn>;
