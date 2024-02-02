07bf51519d27b60e8afb36a12f870f805e5dedb2
sdevalk <sjors@sjorsdevalk.nl>
2024-02-02 16:59:06 +0100

    feat: locale-aware names of publishers and provenance types

--- apps/researcher/src/lib/api/objects/provenance-events-fetcher.integration.test.ts
+++ apps/researcher/src/lib/api/objects/provenance-events-fetcher.integration.test.ts
@@ -59,8 +59,8 @@ describe('getByHeritageObjectId', () => {
             startDate: new Date('1901-01-01T00:00:00.000Z'),
             endDate: new Date('1901-12-31T23:59:59.999Z'),
           },
-          startDate: new Date('1901-01-01T00:00:00.000Z'),
-          endDate: new Date('1901-01-01T00:00:00.000Z'),
+          startDate: new Date('1901-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
+          endDate: new Date('1901-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
           startsAfter:
             'https://example.org/objects/1/provenance/event/5/activity/1',
           endsBefore:
@@ -95,8 +95,8 @@ describe('getByHeritageObjectId', () => {
             startDate: new Date('1879-01-01T00:00:00.000Z'),
             endDate: new Date('1879-12-31T23:59:59.999Z'),
           },
-          startDate: new Date('1879-01-01T00:00:00.000Z'),
-          endDate: new Date('1879-01-01T00:00:00.000Z'),
+          startDate: new Date('1879-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
+          endDate: new Date('1879-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
           startsAfter:
             'https://example.org/objects/1/provenance/event/2/activity/1',
           endsBefore:
@@ -138,8 +138,8 @@ describe('getByHeritageObjectId', () => {
             startDate: new Date('1939-01-01T00:00:00.000Z'),
             endDate: new Date('1940-12-31T23:59:59.999Z'),
           },
-          startDate: new Date('1939-01-01T00:00:00.000Z'),
-          endDate: new Date('1940-01-01T00:00:00.000Z'),
+          startDate: new Date('1939-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
+          endDate: new Date('1940-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
           startsAfter:
             'https://example.org/objects/1/provenance/event/3/activity/1',
           location: {
@@ -172,8 +172,8 @@ describe('getByHeritageObjectId', () => {
             startDate: new Date('1879-01-01T00:00:00.000Z'),
             endDate: new Date('1879-12-31T23:59:59.999Z'),
           },
-          startDate: new Date('1879-01-01T00:00:00.000Z'),
-          endDate: new Date('1879-01-01T00:00:00.000Z'),
+          startDate: new Date('1879-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
+          endDate: new Date('1879-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
           startsAfter:
             'https://example.org/objects/1/provenance/event/1/activity/1',
           endsBefore:
@@ -219,8 +219,8 @@ describe('getByHeritageObjectId', () => {
             startDate: new Date('1855-01-01T00:00:00.000Z'),
             endDate: new Date('1857-12-31T23:59:59.999Z'),
           },
-          startDate: new Date('1855-01-01T00:00:00.000Z'),
-          endDate: new Date('1857-01-01T00:00:00.000Z'),
+          startDate: new Date('1855-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
+          endDate: new Date('1857-01-01T00:00:00.000Z'), // For BC; remove when prop date is in use
           endsBefore:
             'https://example.org/objects/1/provenance/event/2/activity/1',
           location: {
