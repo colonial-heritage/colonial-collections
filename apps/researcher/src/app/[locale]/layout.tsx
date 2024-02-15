6e6441366fde2d6ae9ec189c0534cef07c200955
Bas Doppen <basdoppen@gmail.com>
2024-02-15 11:52:18 +0100

    add favicon remove head.tsx

--- apps/researcher/src/app/[locale]/layout.tsx
+++ apps/researcher/src/app/[locale]/layout.tsx
@@ -3,11 +3,16 @@ import {ReactNode} from 'react';
 import {notFound} from 'next/navigation';
 import {NextIntlClientProvider} from 'next-intl';
 import {ClerkProvider} from '@clerk/nextjs';
+import {Metadata} from 'next';
 
 interface Props {
   children: ReactNode;
   params: {locale: string};
 }
+
+export const metadata: Metadata = {
+  title: 'Colonial Collections Datahub | Collonial Collecions Consortium',
+};
 
 export default async function RootLayout({children, params: {locale}}: Props) {
   let messages;
@@ -21,6 +26,7 @@ export default async function RootLayout({children, params: {locale}}: Props) {
 
   return (
     <ClerkProvider localization={clerkLocale}>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <html lang={locale}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
