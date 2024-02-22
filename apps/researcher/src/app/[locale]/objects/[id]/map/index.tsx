1cff184b33d91622d31b2084e175b5ba75c75055
Barbara Honhoff <barbara@honhoff.dev>
2024-02-22 13:49:09 +0100

    Add a map to the object details

---
+++ apps/researcher/src/app/[locale]/objects/[id]/map/index.tsx
@@ -0,0 +1,45 @@
+import {PostalAddress} from '@/lib/api/definitions';
+import dynamic from 'next/dynamic';
+
+// Disable server-side rendering for the map component,
+// because this component uses the window object.
+const Map = dynamic(() => import('./map'), {
+  ssr: false,
+});
+
+interface Props {
+  address?: PostalAddress;
+}
+
+export default async function MapByAddress({address}: Props) {
+  if (!address) {
+    return null;
+  }
+
+  try {
+    const addressString = [
+      address.streetAddress,
+      address.postalCode,
+      address.addressLocality,
+      address.addressCountry,
+    ]
+      .filter(n => n)
+      .join(',');
+
+    // The map component needs longitude and latitude to display the location.
+    // We can use the OpenStreetMap API to get these coordinates from the address.
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+        addressString
+      )}`
+    );
+
+    const responseData = await response.json();
+    const location = responseData[0];
+
+    return <Map lat={location.lat} lon={location.lon} />;
+  } catch (error) {
+    console.error('Error fetching location data:', error);
+    return null;
+  }
+}
