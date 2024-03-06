f6c22a40c1451a1c11b4c3944cfa1e8c6e49a443
Barbara Honhoff <barbara@honhoff.dev>
2024-03-06 10:53:37 +0100

    Set dataset-browser 'type' to 'module'

--- apps/dataset-browser/src/i18n.ts
+++ apps/dataset-browser/src/i18n.ts
@@ -1,5 +1,17 @@
+import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
+import {locales} from './navigation';
+import {LocaleEnum} from '@/definitions';
 
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`./messages/${locale}/messages.json`)).default,
-}));
+export default getRequestConfig(async ({locale}) => {
+  if (!locales.includes(locale as LocaleEnum)) notFound();
+
+  return {
+    messages: (
+      await (locale === 'en'
+        ? // This will enable HMR for `en`
+          import('./messages/en/messages.json')
+        : import(`./messages/${locale}/messages.json`))
+    ).default,
+  };
+});
--- apps/dataset-browser/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-const sharedConfig = require('tailwind-config/tailwind.config.ts');
-
-module.exports = {
-  presets: [sharedConfig],
-};
