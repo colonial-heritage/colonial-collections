6836948b6bf92fa927f05c27a74c85e06e00eed0
Barbara Honhoff <barbara@honhoff.dev>
2024-05-03 15:49:53 +0200

    Refactor dataset browser end to end tests

--- apps/dataset-browser/playwright.config.ts
+++ apps/dataset-browser/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: env.PLAYWRIGHT_BASE_URL || 'http://localhost:3001',
+    baseURL: env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -30,22 +30,16 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'global setup',
-      testMatch: /global\.setup\.ts/,
-    },
-    {
       name: 'Desktop Chrome',
       use: {
         ...devices['Desktop Chrome'],
       },
-      dependencies: ['global setup'],
     },
     {
       name: 'Firefox',
       use: {
         ...devices['Desktop Firefox'],
       },
-      dependencies: ['global setup'],
     },
   ],
 });
