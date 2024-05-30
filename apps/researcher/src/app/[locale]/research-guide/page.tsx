43f25923591873487cc31a399a4ba7d8289aacff
Barbara Honhoff <barbara@honhoff.dev>
2024-05-30 09:22:44 +0200

    Add temp page for research guide

---
+++ apps/researcher/src/app/[locale]/research-guide/page.tsx
@@ -0,0 +1,8 @@
+export default function ResearchGuidePage() {
+  return (
+    <div>
+      <h1>Research Guide</h1>
+      <p>The research guide will be added soon. Please check back later.</p>
+    </div>
+  );
+}
