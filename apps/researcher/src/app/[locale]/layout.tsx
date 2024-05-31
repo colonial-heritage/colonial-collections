012d28e3747ba4ab446f958746c79cc8088c0969
Barbara Honhoff <barbara@honhoff.dev>
2024-05-31 15:35:05 +0200

    simplify the layout structure

--- apps/researcher/src/app/[locale]/layout.tsx
+++ apps/researcher/src/app/[locale]/layout.tsx
@@ -4,6 +4,11 @@ import {notFound} from 'next/navigation';
 import {NextIntlClientProvider} from 'next-intl';
 import {ClerkProvider} from '@clerk/nextjs';
 import {getTranslations} from 'next-intl/server';
+import Navigation from '@/components/navigation';
+import {WipMessage} from '@colonial-collections/ui';
+import {Link} from '@/navigation';
+import {env} from 'node:process';
+import AuthHealthCheck from '@/lib/auth-health-check';
 
 interface Props {
   children: ReactNode;
@@ -23,6 +28,8 @@ export async function generateMetadata({params: {locale}}: MetadataProps) {
 }
 
 export default async function RootLayout({children, params: {locale}}: Props) {
+  const t = await getTranslations({locale, namespace: 'Meta'});
+
   let messages;
   try {
     messages = (await import(`../../messages/${locale}/messages.json`)).default;
@@ -36,9 +43,30 @@ export default async function RootLayout({children, params: {locale}}: Props) {
     <ClerkProvider localization={clerkLocale}>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <html lang={locale}>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-        </NextIntlClientProvider>
+        <body>
+          <AuthHealthCheck />
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <WipMessage Link={Link} />
+
+            <div className="sr-only">
+              <ul>
+                <li>
+                  <a href="#facets">{t('jumpFilters')}</a>
+                </li>
+                <li>
+                  <a href="#search-results">{t('jumpResults')}</a>
+                </li>
+                <li>
+                  <a href="#page-navigation">{t('jumpNavigation')}</a>
+                </li>
+              </ul>
+            </div>
+            <header className="w-full bg-consortium-blue-900 text-white py-2">
+              <Navigation datasetBrowserUrl={env['DATASET_BROWSER_URL']!} />
+            </header>
+            {children}
+          </NextIntlClientProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
--- apps/researcher/src/app/layout.tsx
+++ apps/researcher/src/app/layout.tsx
@@ -7,6 +7,6 @@ type Props = {
 // Even though this component is just passing its children through, the presence
 // of this file fixes an issue in Next.js 13.4 where link clicks that switch
 // the locale would otherwise cause a full reload.
-export default function RootLayout({children}: Props) {
+export default async function RootLayout({children}: Props) {
   return children;
 }
