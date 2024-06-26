95b30423c65e1f8ce112243b563829e7f4c1909d
Barbara Honhoff <barbara@honhoff.dev>
2024-06-26 14:29:49 +0200

    Fix end-to-end tests

--- apps/researcher/e2e/object.test.ts
+++ apps/researcher/e2e/object.test.ts
@@ -94,7 +94,7 @@ test.describe('Object details page logged in', () => {
       .getByTestId('enrichment-form')
       .locator('button[type="submit"]')
       .click();
-    await expect(page.getByTestId('notification')).toHaveCount(2, {
+    await expect(page.getByTestId('notification')).toHaveCount(3, {
       timeout: 30000,
     });
 
