bd92ec0bf2b893792e9586b76e3c5c25e09e6309
Barbara Honhoff <barbara@honhoff.dev>
2025-07-11 11:48:24 +0200

    Remove temp iri check page

--- apps/researcher/src/app/[locale]/iri-check/layout.tsx
+++
@@ -1,5 +0,0 @@
-import {ReactNode} from 'react';
-
-export default function Layout({children}: {children: ReactNode}) {
-  return <main className="w-full px-4 sm:px-10 max-w-7xl">{children}</main>;
-}
