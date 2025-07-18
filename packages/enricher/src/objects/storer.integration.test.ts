26140baa1fc47fab7973f65460108f03b51f1d92
sdevalk <sjors@sjorsdevalk.nl>
2025-07-18 12:54:45 +0200

    feat: replace client

--- packages/enricher/src/objects/storer.integration.test.ts
+++ packages/enricher/src/objects/storer.integration.test.ts
@@ -1,4 +1,4 @@
-import {NanopubClient} from '../client';
+import {NanopubClient} from '../native-client';
 import {HeritageObjectEnrichmentType} from './definitions';
 import {HeritageObjectEnrichmentStorer} from './storer';
 import {describe, expect, it} from '@jest/globals';
@@ -6,7 +6,7 @@ import {env} from 'node:process';
 
 const nanopubClient = new NanopubClient({
   endpointUrl: env.NANOPUB_WRITE_ENDPOINT_URL as string,
-  proxyEndpointUrl: env.NANOPUB_WRITE_PROXY_ENDPOINT_URL as string,
+  privateKey: env.NANOPUB_PRIVATE_KEY as string,
 });
 
 const storer = new HeritageObjectEnrichmentStorer({nanopubClient});
